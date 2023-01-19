2fc94f3bb8b21e2db8341fd38d3721d530f4ae17
Isabel <isamdrituerto@hotmail.com>
2023-01-19 16:19:43 +0100

    Added functionality to total count

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,10 +1,17 @@
 const Counter = ({title, count, onButtonClick}) => {
 
+    //functions in a component that handles changes in state
+    
     const handleAddParticipant = () => {
         onButtonClick(count + 1);
         // setArcheryCount(count + 1)
         // setZorbCount(count + 1)
         // setCanoeCount(count + 1)
+    }
+
+    const handleRemoveParticipant = () => {
+        if (count>0){
+            onButtonClick(count - 1);}
     }
     
     return(
@@ -12,7 +19,7 @@ const Counter = ({title, count, onButtonClick}) => {
             <h3>{title}</h3>
             <p>Current total: {count}</p>
             <button onClick={handleAddParticipant}> Add participant</button>
-            <button> Remove participant</button>
+            <button onClick={handleRemoveParticipant}> Remove participant</button>
         </>
     )
 
--- src/containers/SignupContainer.js
+++ src/containers/SignupContainer.js
@@ -3,19 +3,20 @@ import Counter from "../components/Counter";
 
 const SignupContainer = () => {
 
+    //display data using states
     const [archeryCount, setArcheryCount] = useState(0);
     const [zorbCount, setZorbCount] = useState(0);
     const [canoeCount, setCanoeCount] = useState(0);
 
+    // pass down data using "props" in "Counter.js" file
     return(
         <>
         <h2> Activities</h2>
-
         <Counter title={"Archery"} count={archeryCount} onButtonClick={setArcheryCount}/>
         <Counter title={"Zorbing"} count={zorbCount} onButtonClick={setZorbCount}/>
         <Counter title={"Canoeing"} count={canoeCount} onButtonClick={setCanoeCount}/>
 
-        <p>Total sign-ups: coming soon</p>
+        <p>Total sign-ups: {archeryCount+zorbCount+canoeCount}</p>
         </>
     );
 }
